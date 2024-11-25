938f27a033b3edc113cff6fa3153f6d86722ecc9
Jesus <jesus@b25>
2024-11-25 09:50:49 +0100

    Instituto Terminado

--- src/org/ies/highschool/Main.java
+++ src/org/ies/highschool/Main.java
@@ -1,7 +1,12 @@
 package org.ies.highschool;
-
+import org.ies.highschool.components.StudentReader;
+import org.ies.highschool.models.Student;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        StudentReader studentReader = new StudentReader(scanner);
+        Student student = studentReader.read();
+        student.showInfo();
     }
 }
